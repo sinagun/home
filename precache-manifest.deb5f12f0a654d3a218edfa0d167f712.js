5345d1bc87e457bd8a7c136c5c5a4c47be667f20
sinagun <38187655+sinagun@users.noreply.github.com>
2021-12-02 18:45:02 +0330

    Updates

---
+++ precache-manifest.deb5f12f0a654d3a218edfa0d167f712.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72a6f65e7a6a119afb52a6a34a21a5c6",
+    "url": "/home/index.html"
+  },
+  {
+    "revision": "89d85058c6c337210dce",
+    "url": "/home/static/css/main.469993f4.chunk.css"
+  },
+  {
+    "revision": "a86c76c42982948d42be",
+    "url": "/home/static/js/2.445688f3.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/home/static/js/2.445688f3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "89d85058c6c337210dce",
+    "url": "/home/static/js/main.424dacbc.chunk.js"
+  },
+  {
+    "revision": "c0578b487099793a068c",
+    "url": "/home/static/js/runtime-main.3c590061.js"
+  },
+  {
+    "revision": "5cf1e32a5bce852d1dc8b17370906a32",
+    "url": "/home/static/media/Sakhtemankala.5cf1e32a.png"
+  },
+  {
+    "revision": "52b68f70a6f4f4373439b0ac71d24927",
+    "url": "/home/static/media/f1.52b68f70.png"
+  },
+  {
+    "revision": "32f6b574eb11519020e24620a38bdf55",
+    "url": "/home/static/media/hashirshoaeb.32f6b574.png"
+  },
+  {
+    "revision": "9f931475264ea38bb96ea842342f36e7",
+    "url": "/home/static/media/resume.9f931475.pdf"
+  }
+]);
